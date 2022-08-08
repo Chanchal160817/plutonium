d37211a95488e449a303cd9c4731182a446d091d
Chanchal160817 <you@example.com>
2022-08-09 00:28:35 +0530

    message

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,51 +1,111 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
-// const underscore = require('underscore')
-// const chanchal = require('lodash');
+
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
     res.send('My second ever api!')
-     
-    // let weekend = { saturday, sunday }
-    // let result = chanchal.first(weekend)
-    // console.log("unserscore example result is", result);
 
-    // let month = ['january', 'february', 'march', 'appril', 'may', 'jun', 'july', 'aug', 'september', 'october', 'november', 'december'];
-    // let b = chanchal.chunk(month, 4);
-    // console.log(b);
-
-    // let odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-    // let c = chanchal.tail(odd);
-    // console.log(c);
-
-    // let arr1 = [1, 2, 2]
-    // let arr2 = [3, 3, 4, 5]
-    // let arr3 = [5, 5, 6]
-    // let arr4 = [6, 6, 8]
-    // let arr5 = [7, 7, 8, 9];
-    // let d = chanchal.union(arr1, arr2, arr3, arr4, arr5);
-    // console.log(d);
-
-    // let pair = [["horror", "The Shining"], ["drama", "Titanic"], ["thriller", "Shutte Island"], ["fantasy", "Pans Labyrinth"]]
-    // let e = chanchal.fromPairs(pair);
-    // console.log(e);
 });
 
-
-router.get('/test-you', function (req, res) {
-    res.send('This is the second routes implementation')
-})
-
-router.get('/give-me-students-data', function (req, res) {
-
-})
-
-router.get('/movies', function (req, res){
-    let students = ['KGF', 'RRR', 'KGF Chapter 2']
+router.get('/movies', function (req, res) {
+    let movies = ['kgf', 'rrr', 'ddlj']
     res.send(movies)
 })
+
+router.get('/movies/:index', function (req, res) {
+    let movies = ['kgf', 'rrr', 'ddlj']
+
+    let requestParams = req.params
+    let index = JSON.stringify(requestParams)
+    let value = Object.values(requestParams)
+
+    res.send(movies[value])
+})
+
+// router.get('/movies/:i', function (req, res) {
+//     let movies = ['kgf', 'rrr', 'ddlj']
+
+//     let requestParams = req.params.i
+//     let index = JSON.stringify(requestParams)
+//     let value = Object.values(requestParams)
+
+//     if (i >= movies.length) {
+//         res.send("Enter valid number ")
+//     } else {
+//         res.send(movies[i])
+
+    //}
+
+//})
+
+// router.get('/movies/:indexNumber',function(req,res){
+//     let movies = ['kgf', 'rrr', 'ddlj']
+//     let index =req.params.indexNumber;
+//     if(index>movie.length){
+//         res.send("please enetr the valid index number")
+//     }else{
+//         res.send(movie[index])
+//     }
+// })
+
+router.get('/movies/:i',function(req , res){
+    let movies = ['DDLJ','MS.DHONI','KGH2', 'RRR']
+        
+        let requestParams = req.params  //
+        let num= JSON.stringify(requestParams)
+        let value=Object.values(requestParams)
+        
+        if(value >= movies.length ){
+            res.send("Enter Valid Number")
+        }else{
+        res.send(movies[value])
+        }
+    })
+
+router.get('/films', function (req, res) {
+    let films = [{
+        'id': 1,
+        'name': 'The Shining'
+    }, {
+        'id': 2,
+        'name': 'Incendies'
+    }, {
+       ' id': 3,
+        'name': 'Rang de Basanti'
+    }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+    }]
+
+    res.send(films)
+
+
+})
+
+router.get('/films/:filmsid', function (req, res) {
+    let films = [{
+        'id': 1,
+        'name': 'The Shining'
+    }, {
+        'id': 2,
+        'name': 'Incendies'
+    }, {
+       ' id': 3,
+        'name': 'Rang de Basanti'
+    }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+    }]
+    let requestParams = req.params
+    let index = JSON.stringify(requestParams)
+    let filmsid = Object.values(requestParams)
+    res.send(films[filmsid])
+})
+
+
+
 module.exports = router;
 // adding this comment for no reason
