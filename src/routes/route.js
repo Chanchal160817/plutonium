40f3acb52fef2164cdea6d141d7c7ec2dd0a73ff
Chanchal160817 <you@example.com>
2022-08-22 20:18:24 +0530

    assigment of 11 aug

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,22 +7,64 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.get("/sol2", function (req, res) {
-    let arr = [33, 34, 35, 37, 38]
-    let len = arr.length
-
-    let total = 0;
-    for (var i in arr) {
-        total += arr[i];
-    }
-
-    let firstDigit = arr[0]
-    let lastDigit = arr.pop()
-    let consecutiveSum = (len + 1) * (firstDigit + lastDigit) / 2
-    let missingNumber = consecutiveSum - total
-
-    res.send({ data: missingNumber });
-});
+router.post('/players', function (req, res) {
+    let players = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+    ]
+     
+        
+        let newPlayer = req.body
+        let newPlayersName = newPlayer.name
+        let isNameRepeated = false
+     
+        //let player = players.find(p => p.name == newPlayersName)
+     
+        for(let i = 0; i < players.length; i++) {
+            if(players[i].name == newPlayersName) {
+                isNameRepeated = true;
+                break;
+            }
+        }
+     
+        //undefined is same as false/ a falsy value
+        if (isNameRepeated) {
+            //Player exists
+            res.send("This player was already added!")
+        } else {
+            //New entry
+            players.push(newPlayer)
+            res.send(players)
+        }
+    });
+    
+    
+    
 
 
 module.exports = router;
