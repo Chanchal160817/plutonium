72240dbc92605d7482a5adc30316f4d41412ae72
Chanchal160817 <you@example.com>
2022-08-22 19:05:42 +0530

    assignment of 9th aug problem 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,21 +7,22 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.get("/sol1", function (req, res) {
-
-
-    let arr = [1, 2, 3, 5, 6, 7]
+router.get("/sol2", function (req, res) {
+    let arr = [33, 34, 35, 37, 38]
+    let len = arr.length
 
     let total = 0;
     for (var i in arr) {
         total += arr[i];
     }
 
+    let firstDigit = arr[0]
     let lastDigit = arr.pop()
-    let consecutiveSum = lastDigit * (lastDigit + 1) / 2
+    let consecutiveSum = (len + 1) * (firstDigit + lastDigit) / 2
     let missingNumber = consecutiveSum - total
 
     res.send({ data: missingNumber });
 });
+
 
 module.exports = router;
