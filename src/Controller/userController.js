fd4bbef978ccc6146980af4b3324e6d6c03448c3
Chanchal160817 <you@example.com>
2022-08-24 19:24:44 +0530

    update branch

---
+++ src/Controller/userController.js
@@ -0,0 +1,14 @@
+const book = require("../model/userModel")
+const createBook = async function (req, res) {
+    let data= req.body
+    let savedData = await userModel.create(data)
+    res.send({msg: savedData})
+}
+ 
+const getBooksData= async function (req, res) {
+    let allBooks= await userModel.find()
+    res.send({msg: allBooks})
+}
+ 
+module.exports.createBook = createBook
+module.exports.getBooksData = getBooksData
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,69 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-
-
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-let person =[
-    {
-        name : "pk",
-        age :12,
-        votingStatus : false
-    },
-    {
-        name : "sk",
-        age :22,
-        votingStatus : true
-    },
-    {
-        name : "AA",
-        age :72,
-        votingStatus : false
-    },
-    {
-    name : "Sc",
-    age :5,
-    votingStatus : false
-    },
-    {
-    name : "HO",
-    age :42,
-    votingStatus : true
-    }]
-     
-    router.post('/voting' , function (req, res ) {
-        let VotingAge = req.query.age
-        let ElegiblePerson = []
-        for (let i=0; i<person.length; i++)
-        {
-            if(person[i].age>VotingAge){
-                person[i].votingStatus= true;
-                ElegiblePerson.push(person[i])
-     
-            }
-        }
-        res.send({persons: ElegiblePerson , status:true})
-    // inputAge = req.query.votingAge;
-    // person.map(per=>(per.age>inputAge)?per.votingStatus=true:per.votingStatus);
-    // let final=person.filter(pson=>{if(pson.votingStatus==true){return pson.name}});
-    // res.send({data:final});
-     
-    })
-    
-    
-    
-    
-    
-    
-    
-    
-    module.exports = router;
-    
-    
-    
-
-
+const UserModel= require("../model/userModel.js")
+const UserController= require("../controller/userController")
+ 
+router.post("/createBook",UserController.createBook )
+ 
+router.get("/getBooksData", UserController.getBooksData)
+ 
 module.exports = router;
+
+    
+
+
