5d44cf974bca544ae23a9e059376d2e138ae642a
Chanchal160817 <you@example.com>
2022-08-31 13:07:25 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,4 @@
-cconst express = require('express');
+const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/userController")
 const mware = require("../middleware/auth")
