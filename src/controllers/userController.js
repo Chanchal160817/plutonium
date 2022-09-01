eb892f2f5f11ba1e0861474364df108f12e3d490
Chanchal160817 <you@example.com>
2022-09-01 15:26:46 +0530

    changes in status

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,10 +7,11 @@ try {
     let data = abcd.body;
     let savedData = await userModel.create(data);
     console.log(abcd.newAtribute);
-    xyz.send({ msg: savedData });
+    xyz.status(201).send({ msg: savedData });
   }
  catch (error) {
   console.log(error.message);
+  res.status(500).send({ msg: "Error", error: err.message })
 }
 };
 const loginUser = async function (req, res) {
@@ -20,7 +21,7 @@ try {
   
     let user = await userModel.findOne({ emailId: userName, password: password });
     if (!user)
-      return res.send({
+      return res.status(404).send({
         status: false,
         msg: "username or the password is not corerct",
       });
@@ -35,9 +36,10 @@ try {
       "functionup-thorium"
     );
     res.setHeader("x-auth-token", token);
-    res.send({ status: true, data: token });
+    res.status(200).send({ status: true, data: token });
 } catch (error) {
   console.log(error.message);
+  res.status(500).send({ msg: "Error", error: err.message })
 }
 };
 
@@ -48,12 +50,13 @@ try {
     let userId = req.params.userId;
     let userDetails = await userModel.findById(userId);
     if (!userDetails)
-      return res.send({ status: false, msg: "No such user exists" });
+      return res.status(400).send({ status: false, msg: "No such user exists" });
   
-    res.send({ status: true, data: userDetails });
+    res.status(200).send({ status: true, data: userDetails });
 
 } catch (error) {
   console.log(error.message);
+  res.status(500).send({ msg: "Error", error: err.message })
 }
 };
 
@@ -64,14 +67,15 @@ try {
     let user = await userModel.findById(userId);
     //Return an error if no user with the given id exists in the db
     if (!user) {
-      return res.send("No such user exists");
+      return res.status(400).send("No such user exists");
     }
   
     let userData = req.body;
     let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-    res.send({ status: true, data: updatedUser });
+    res.status(200).send({ status: true, data: updatedUser });
 } catch (error) {
   console.log(error.message);
+  res.status(500).send({ msg: "Error", error: err.message })
 }
 };
 
@@ -81,13 +85,14 @@ try {
     let userId = req.params.userId
     let user = await userModel.findById(userId)
     if (!user) {
-      return res.send({ status: false, message: "no such user exists" })
+      return res.status(400).send({ status: false, message: "no such user exists" })
     }
     let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, { isDeleted: true }, { new: true })
-    res.send({ status: true, data: updatedUser })
+    res.status(200).send({ status: true, data: updatedUser })
   
 } catch (error) {
   console.log(error.message);
+  res.status(500).send({ msg: "Error", error: err.message })
 }
 };
 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,12 +6,12 @@ try {
         let token = req.headers["x-Auth-token"];
         if (!token) token = req.headers["x-auth-token"];
         //If no token is present in the request header return error
-        if (!token) return res.send({ status: false, msg: "token must be present" });
+        if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
         console.log(token);
     
         let decodedToken = jwt.verify(token, "functionup-thorium");
         if (!decodedToken) {
-            return res.send({ status: false, msg: "token is invalid" })
+            return res.status(401).send({ status: false, msg: "token is invalid" })
         }
         else {
     
@@ -35,7 +35,7 @@ try {
         let userLoggedIn = decodedToken.userId
     
         //userId comparision to check if the logged-in user is requesting for their own data
-        if (userToBeModified != userLoggedIn) return res.send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
+        if (userToBeModified != userLoggedIn) return res.status(403).send({ status: false, msg: 'User logged is not allowed to modify the requested users data' })
         next()
     
     
