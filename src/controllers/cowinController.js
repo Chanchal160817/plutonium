73fe1f5de55440fdababc9ad307af075322cf4b8
Chanchal160817 <you@example.com>
2022-09-01 21:25:17 +0530

    sort city as temp

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,104 +1,26 @@
 let axios = require("axios")
+let getLondanTemp =async function(req,res){
 
+        try {
+            let cities = ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+            let cityObjArray =[]
 
-let getStates = async function (req, res) {
+        for (let i = 0; i < cities.length; i++) {
+            let object= { city : cities[i]}
+            let url = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=5cc42eb5f2bdd96470675caebd4bc027
+            `)
+            console.log(url.data.main.temp);
+            object.temp = url.data.main.temp
+            cityObjArray.push(object)
+            }
 
-    try {
-        let options = {
-            method: 'get',
-            url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
-        }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-
-let getDistricts = async function (req, res) {
-    try {
-        let id = req.params.stateId
-        let options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
-        }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-let getByPin = async function (req, res) {
-    try {
-        let pin = req.query.pincode
-        let date = req.query.date
-        console.log(`query params are: ${pin} ${date}`)
-        var options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
-        }
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-let getOtp = async function (req, res) {
-    try {
-        let blahhh = req.body
-
-        console.log(`body is : ${blahhh} `)
-        var options = {
-            method: "post",
-            url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
-            data: blahhh
+            let sorted = cityObjArray.sort(function(a ,b){return a.temp-b.temp})
+            console.log(sorted);
+            res.status(200).send({status : true , data: sorted})
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).send({status:false , msg: 'server error'})
         }
 
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-let getList = async function (req, res) {
-    try {
-        let district = req.query.district_id
-        let date = req.query.date
-        console.log(`query param are  : ${district} ${date} `);
-        var options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}&date=${date}`
-        }
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({ msg: error.message })
-    }
-}
-
-
-module.exports.getStates = getStates
-module.exports.getDistricts = getDistricts
-module.exports.getByPin = getByPin
-module.exports.getOtp = getOtp
-module.exports.getList = getList
+};
+module.exports.getLondanTemp = getLondanTemp
